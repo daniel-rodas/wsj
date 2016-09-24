6acd7701c337d28bcd59200a4ee26323ada543ed
Daniel Rodas <daniel.rodas1@hotmail.com>
2016-09-23 23:49:30 -0700

    removing files from version control

--- public/app/app.module.ts
+++ public/app/app.module.ts
@@ -6,9 +6,9 @@ import { NgbModule }            from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent }         from './app.component';
 import { NavigationComponent }  from './navigation.component';
 import { WhatsNewsComponent }   from './components/whatsNews/whats.news.component';
-//, NgbModule
+//,
 @NgModule({
-    imports:      [ BrowserModule, FormsModule  ],
+    imports:      [ NgbModule, BrowserModule, FormsModule  ],
     declarations: [ AppComponent, NavigationComponent, WhatsNewsComponent  ],
     bootstrap:    [ AppComponent, NavigationComponent, WhatsNewsComponent ]
 })
