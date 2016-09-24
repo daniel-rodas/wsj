64d35698e4f06a6a2b5b4c1b3201d960c54f8a9e
Daniel Rodas <daniel.rodas1@hotmail.com>
2016-09-24 16:53:51 -0700

    Related model User is null in Post model, causes an error whe displaying page.

--- fuel/app/modules/blog/views/frontend/post/show.php
+++ fuel/app/modules/blog/views/frontend/post/show.php
@@ -3,8 +3,11 @@
     <h1><?= $post->name; ?></h1>
     <p>
     	<small>
-			<?= __('by'); ?> <a href="<?= \Router::get('show_post_author', array('author' => $post->user->username)); ?>">
-                <?= $post->user->username; ?></a> <?= __('on'); ?> <em><?= date('d/m/Y', $post->created_at); ?></em>
+            <pre>
+                <?=  var_dump($post->user) ?>
+            </pre>
+			<?= __('by'); ?> <a href="<?= \Router::get('show_post_author', array('author' => 'Bob Zaneta.')); ?>">
+                Bob Zaneta.</a> <?= __('on'); ?> <em><?= date('d/m/Y', $post->created_at); ?></em>
 		 </small>
 	 </p>
     </div>
