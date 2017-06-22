4f8540f0b59819e2ac2682f660bf8a0a3e00f62d
Daniel Rodas <daniel.rodas1@hotmail.com>
2017-06-22 16:22:38 -0700

    remove ts files for server

--- public/app/common/header/navigation.user.setting.option.dropdown.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-    selector: 'rn-navigation-user-setting-option-dropdown',
-    templateUrl: '/angular/template/navigation_user_setting_option_dropdown'
-})
-export class NavigationUserSettingOptionDropdown {
-}
